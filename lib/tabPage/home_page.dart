0c4ab3e696a5b59ddb30662d42a20a7b8dd823e2
Tosakan <hurricanenoahzau@gmail.com>
2021-03-25 17:27:06 +0700

    no message

--- lib/tabPage/home_page.dart
+++ lib/tabPage/home_page.dart
@@ -12,114 +12,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Widget> _getButtonList() {
     List<Widget> _buttonList = [
-      Container(
-          child: OutlinedButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return WeaponShop();
-          }));
-        },
-        child: Text("Weapon Shop"),
-      )),
-      Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Potion Shop"),
-      )),
-      Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Magic Shop"),
-      )),
-      Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),Container(
-          child: OutlinedButton(
-        onPressed: () {
-          // Respond to button press
-        },
-        child: Text("Corps Base"),
-      )),
+      Stack(
+        children: <Widget>[
+          Container(
+            child: Image(
+              width: MediaQuery.of(context).size.width,
+              image: NetworkImage('https://i.ytimg.com/vi/yrKLRI2GrJM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDP-XO0zaKUda59K908hY5VvUb06Q'),
+            ),
+          ),
+          Positioned(
+            right: 5.0,
+            bottom: 5.0,
+            child: Container(
+              decoration: new BoxDecoration (
+                borderRadius: new BorderRadius.all(new Radius.circular(2.0)),
+                color: const Color(0xFF282828).withOpacity(0.8)
+              ),
+              padding: new EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+              child: Text("1:00:01",
+                style: TextStyle(fontSize: 14, color: Colors.white)
+              ),
+            )
+          ),
+        ]
+      ),
     ];
     return _buttonList;
   }
